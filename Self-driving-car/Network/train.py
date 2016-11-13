762d2bff14f6322332c473af84bdeb0dd6f41454
astaroth106 <andrescapc@aol.com>
2016-11-13 12:04:02 -0600

    Update train.py

--- Self-driving-car/Network/train.py
+++ Self-driving-car/Network/train.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
 		os.makedirs(checkpoint_path)
 
 	model = build_model()
-	model.load('road_model1-400000')
+	model.load('road_model1-800000')
 
 	# Train
 	for i in range(num_iterations):
